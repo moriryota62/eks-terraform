e86645ba89c7b5bd830603ee4f6d614aed1a88c7
mouriryouta <moriryota62@gmail.com>
2021-10-11 16:16:42 +0900

    #45 Container Insightsによるログ監視

--- terraform/container-insights-log/import.tf
+++ terraform/container-insights-log/import.tf
@@ -1,13 +1,3 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-
-  config = {
-    bucket = "project-environment-tfstate"
-    key    = "network/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
-
 data "terraform_remote_state" "eks" {
   backend = "s3"
 
